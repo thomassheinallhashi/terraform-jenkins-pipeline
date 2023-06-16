8608d68c12d1e2f46a493db1c4b4081aefcee47a
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2023-06-16 15:20:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,14 +2,6 @@ variable "aws_access_key" {
     description = "AWS Access Key"
 }
 
-variable "aws_secret_key" {
-    description = "AWS Secret Key"
-}
-
-variable "aws_session_token" {
-    description = "AWS Session Token"
-}
-
 variable "aws_region" {
     description = "AWS Region"
     default = "us-east-1"
