fc63d5b8e61eb09ee683856977e5c7ad5cd7c1fb
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2023-07-16 23:27:02 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_access_key" {
     description = "AWS Access Key"
+    default = " "
 }
 
 variable "aws_secret_key" {
     description = "AWS Secret Key"
+    default = " "
 }
 
 variable "aws_region" {
